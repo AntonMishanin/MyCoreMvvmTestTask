12b4691483fddad06ed79c9bd26c8849cb435beb
Anton Mishanin <antonmishanin@mail.ru>
2022-06-09 10:23:39 +0700

    init project

---
+++ app/src/main/java/com/my/mycoremvvmtesttask/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.my.mycoremvvmtesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.johnnysc.coremvvm.presentation.BackPress
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
